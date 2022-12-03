70343f85cdf1a76a81eee19aa273d782ca18aa46
Terminator15474 <jandommert8@gmail.com>
2022-12-03 20:26:58 +0100

    initial commit

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bestellplatform.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
